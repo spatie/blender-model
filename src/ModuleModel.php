09558a5f1d2df693615052056785d3e591cee47c
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-10-18 14:51:24 +0200

    Module classes

---
+++ src/ModuleModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Spatie\Blender\Model;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia\Interfaces\HasMediaConversions;
+use Spatie\ModelCleanup\GetsCleanedUp;
+use Spatie\Translatable\HasTranslations;
+
+abstract class ModuleModel extends Model implements HasMediaConversions, GetsCleanedUp
+{
+    use HasTranslations;
+    use Traits\Draftable;
+    use Traits\HasMedia;
+    use Traits\HasSeoValues;
+
+    protected $guarded = ['id'];
+
+    public function registerMediaConversions()
+    {
+        $this->addMediaConversion('admin')
+            ->setWidth(368)
+            ->setHeight(232)
+            ->nonQueued();
+
+        $this->addMediaConversion('redactor')
+            ->setWidth(368)
+            ->setHeight(232)
+            ->nonQueued();
+    }
+
+    public static function cleanUp(Builder $query): Builder
+    {
+        return $query
+            ->draft()
+            ->where('created_at', '<', Carbon::now()->subWeek());
+    }
+
+    public function defaultSeoValues(): array
+    {
+        return [
+            'title' => $this->name,
+            'meta_description' => (string) string($this->text)->tease(155),
+            'meta_og:title' => $this->name,
+            'meta_og:type' => 'website',
+            'meta_og:description' => (string) string($this->text)->tease(155),
+            'meta_og:image' => $this->hasMedia('images') ?
+                url($this->getFirstMediaUrl('images')) :
+                url('/images/og-image.png'),
+        ];
+    }
+}
---
+++ src/Traits/HasSeoValues.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Spatie\Blender\Model\Traits;
+
+trait HasSeoValues
+{
+    /**
+     * @param string|null $key
+     *
+     * @return string|\Illuminate\Support\Collection
+     */
+    public function seo(string $key = null)
+    {
+        if (is_null($key)) {
+            return collect($this->defaultSeoValues())
+                ->merge(collect($this->seo_values ?? [])->filter());
+        }
+
+        return $this->seo()->get($key);
+    }
+
+    public function renderSeoTags(): string
+    {
+        return $this->seo()->filter(function ($value, $key) {
+            return starts_with($key, 'meta_');
+        })->map(function ($value, $key) : string {
+            $key = substr($key, 5);
+            $attribute = starts_with($key, 'og:') ? 'property' : 'name';
+
+            return "<meta {$attribute}=\"{$key}\" content=\"{$value}\">";
+        })->implode("\n");
+    }
+}

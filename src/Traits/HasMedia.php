07e7af2ad61cd3192d407a63cd864b16151b2876
freek <freek@spatie.be>
2017-01-05 15:16:29 +0100

    do not display temp media on front site

--- src/Traits/HasMedia.php
+++ src/Traits/HasMedia.php
@@ -2,14 +2,18 @@
 
 namespace Spatie\Blender\Model\Traits;
 
+use Illuminate\Support\Collection;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\Media;
 
 /**
  * Don't forget to set protected $mediaLibraryCollections.
  */
 trait HasMedia
 {
-    use HasMediaTrait;
+    use HasMediaTrait {
+        getMedia as traitGetMedia;
+    }
 
     public function mediaLibraryCollectionType(string $name): string
     {
@@ -18,6 +22,19 @@ trait HasMedia
 
     public function mediaLibraryCollectionNames(): array
     {
-        return array_keys($this->mediaLibraryCollections) ?? [];
+        return $this->mediaLibraryCollections ?? [];
+    }
+
+    public function getMedia(string $collectionName = '', $filters = []): Collection
+    {
+        $media = $this->traitGetMedia($collectionName, $filters);
+
+        if (request()->isFront()) {
+            $media = $media->reject(function (Media $media) {
+                return $media->getCustomProperty('temp', false);
+            });
+        }
+
+        return $media;
     }
 }

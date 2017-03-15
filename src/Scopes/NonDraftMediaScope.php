0514d3c1ca1b21738b9515dd347cd1c23f52c60b
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-03-15 10:37:05 +0100

    Use a regex in NonDraftMediaScope for sqlite compat

--- src/Scopes/NonDraftMediaScope.php
+++ src/Scopes/NonDraftMediaScope.php
@@ -11,9 +11,7 @@ class NonDraftMediaScope implements Scope
     public function apply(Builder $builder, Model $model)
     {
         if (request()->isFront()) {
-            $builder
-                ->whereRaw('NOT json_contains_path(`custom_properties` ,"one", "$.draft")')
-                ->orWhere('custom_properties->draft', false);
+            $builder->where('custom_properties', 'not regexp', '[^"]*"draft"[^:]*:[^"]*true');
         }
     }
 }
